574e00259940ca1643480a2d6c84414a646434d6
szczepanskisz <szymon.szczepanski@all-for-one.com>
2025-07-02 15:14:28 +0200

    Show real board games

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
 import './App.css'
-import CardSplitHovers from './Card/Card'
+import Card from './Card/Card'
+
+const cardsData = [
+  {
+    title: "Tawantinsuyu",
+    image:
+      "https://sklep.portalgames.pl/userdata/public/gfx/5194/Tawantinsuyu_box_sklep.jpg",
+    description:
+      "Jeszcze w to nie graliśmy",
+  },
+  {
+    title: "Robin z Locksley",
+    image:
+      "https://aleplanszowki.pl/22678-medium_default/robin-z-locksley.jpg",
+    description:
+      "Jeszcze nie graliśmy",
+  },
+  {
+    title: "Dixit",
+    image:
+      "https://aleplanszowki.pl/26351-medium_default/dixit.jpg",
+    description:
+      "Mamy juz wszystkie cześći Dixita. Więc nie chcemy też żadnych dodatków do tego.",
+  },
+];
 
 function App() {
   return (
     <>
-      <h1>Planszówki które już mamy</h1>
-      <CardSplitHovers />
+      <h1>Te planszówki już mamy więc jeśli chcecie nas obdarować jakąś to poprosimy INNĄ niż te tutaj :-)</h1>
+      <Card
+        cardsData={cardsData}
+      />
     </>
   )
 }
--- src/Card/Card.tsx
+++ src/Card/Card.tsx
@@ -2,28 +2,20 @@ import { useEffect, useRef } from "react";
 import Splitting from "splitting";
 import "splitting/dist/splitting.css";
 import "./Card.css";
-// import "./Card.module.css";
+import styles from "./Card.module.css";
 
-const cardsData = [
-  {
-    title: "The City",
-    image:
-      "https://images.unsplash.com/photo-1535498730771-e735b998cd64?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-    alt: "A City skyline at sunset",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos excepturi nostrum necessitatibus doloremque? Quasi non molestias odio. Quasi non molestias odio.",
-  },
-  {
-    title: "The Beach",
-    image:
-      "https://images.unsplash.com/photo-1586500036706-41963de24d8b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-    alt: "A beach at sunset",
-    description:
-      "Quasi non molestias odio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos excepturi nostrum necessitatibus doloremque? Quasi non molestias odio.",
-  },
-];
+interface CardDetails {
+  title: string;
+  image: string;
+  alt?: string;
+  description: string;
+}
 
-const CardSplitHovers = () => {
+interface CardProps {
+  cardsData: CardDetails[];
+}
+
+const Card = ({ cardsData }: CardProps) => {
   const firstCardRef = useRef(null);
 
   useEffect(() => {
@@ -48,7 +40,7 @@ const CardSplitHovers = () => {
           key={idx}
           ref={idx === 0 ? firstCardRef : null}
         >
-          <img src={card.image} alt={card.alt} />
+          <img src={card.image} alt={card.alt ?? card.title} />
           <div className="text">
             <h2 data-splitting="">{card.title}</h2>
             <p data-splitting="">{card.description}</p>
@@ -59,4 +51,4 @@ const CardSplitHovers = () => {
   );
 };
 
-export default CardSplitHovers;
+export default Card;
